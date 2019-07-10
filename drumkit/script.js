de82dccd1428990a8bd8b33e01c3363a7516d607
gabrieljim <gabrieljim641@gmail.com>
2019-07-10 09:24:41 -0400

    Some styling and changed audio elements to be loaded on JS rather than ont h HTML

--- drumkit/script.js
+++ drumkit/script.js
@@ -1,20 +1,42 @@
+const kick = new Audio("./sounds/606-kick1.wav");
+const hihat = new Audio("./sounds/606-cymbal8.wav");
+const snare = new Audio("./sounds/606-snare2.wav");
+const crash = new Audio("./sounds/606-cymbal2.wav")
+const tom1 = new Audio("./sounds/606-tom1.wav")
+const tom2 = new Audio("./sounds/606-tom2.wav")
+const tom3 = new Audio("./sounds/606-tom3.wav")
+
+keys = {
+	102:snare,
+	103:snare,
+	107:hihat,
+	98:kick,
+	110:kick,
+	101:crash,
+	114:tom1,
+	116:tom1,
+	115:tom2,
+	100:tom2,
+	104:tom3,
+	106:tom3,
+}
+
 function audioThings(key) {
-	var audio = document.getElementById(key);
-	var div = document.getElementById(`k${key}`);
+	var div = document.getElementById(key);
+
+
 	div.classList.add("playing");
 	setTimeout(() => div.classList.remove("playing"), 50);
-	console.log(div.classList);
 
-	if (audio.paused) {
-		audio.play();
+	if (keys[key].paused) {
+		keys[key].play();
 	}
 	else {
-		audio.currentTime=0;
+		keys[key].currentTime=0;
 	}
 }
 
 function handleKey(event) {
-	console.log(event.charCode);
 	var key = event.charCode;
 	audioThings(key);
 }
